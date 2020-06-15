b3233bf011e5944e6ab9cf6bbd5133c2da48fa6a
Gabriel Barreto <gabriel.barret015@hotmail.com>
2020-06-15 19:41:03 -0300

    add campos de createdAt em todas as tabelas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import routes from "./routes";
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 app.use(routes);
 
 app.listen(3333);
--- src/routes.js
+++ src/routes.js
@@ -3,27 +3,30 @@ import express from "express";
 const routes = express.Router();
 
 import UserController from "./controllers/UserController";
-
 import ClientController from "./controllers/ClientController";
-
 import AddressController from "./controllers/AddressController";
 import ProductController from "./controllers/ProductController";
 import CategoryController from "./controllers/CategoryController";
 
+// Rotas para clientes
 routes.get("/clients", ClientController.index);
 routes.post("/clients", ClientController.create);
 routes.delete("/clients/:id", ClientController.delete);
 routes.put("/clients/:id", ClientController.update);
 
+// Rotas para endereços
 routes.post("/address/:id", AddressController.create);
 routes.delete("/address/:id", AddressController.delete);
 routes.get("/address", AddressController.index);
 
+// Rotas para produtos
 routes.post("/products", ProductController.create);
 
+// Rotas para categorias
 routes.post("/category", CategoryController.create);
 routes.get("/category", CategoryController.index);
 
+// Rotas para usuários
 routes.get("/users", UserController.index);
 routes.post("/users", UserController.create);
 
